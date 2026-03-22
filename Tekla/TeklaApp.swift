375ddc7e2ef8364be0a366be1cc600bb498be842
Ale <soyelale@outlook.com>
2026-03-22 15:47:16 -0300

    Initial Commit

---
+++ Tekla/TeklaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeklaApp.swift
+//  Tekla
+//
+//  Created by Alejandro on 22/3/26.
+//
+
+import SwiftUI
+
+@main
+struct TeklaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
